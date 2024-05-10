64d2573cc9d13a58f9fabd902658ae4a8797ea0f
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-10 16:26:53 +0200

    feat: get trip-lenght from trip-range and reset

--- main.js
+++ main.js
@@ -1,3 +1,21 @@
 'use strict'
 
-console.log('test')
+//get value from trip-range and print it to trip-leght
+let tripLengh = document.getElementById('trip-lenght');
+let tripRange = document.getElementById('trip-range');
+tripLengh.innerText=tripRange.value
+
+//change trip-lenght on change of trip-range
+tripRange.addEventListener('change',function(){
+
+    tripLengh.innerText=tripRange.value;
+});
+
+document.getElementById('btn-reset').addEventListener('click', function(event){
+   
+    event.preventDefault();
+    tripRange.value=1
+    tripLengh.innerText=tripRange.value;
+})
+
+
