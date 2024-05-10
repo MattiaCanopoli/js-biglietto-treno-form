625a51cd79ced3219ad283e918e69b3a585af04a
MattiaCanopoli <canopoli.mattia@gmail.com>
2024-05-10 18:42:35 +0200

    feat: calculate trip cost on submit and show on page

--- main.js
+++ main.js
@@ -1,21 +1,76 @@
-'use strict'
+'use strict';
+
+//-----FORM VARIABLES-----
+
+
+const age = document.getElementById('age');
+const firstName = document.getElementById('first-name');
+const surname = document.getElementById('surname');
+const tripLengh = document.getElementById('trip-lenght');
+const tripRange = document.getElementById('trip-range');
+
+
+//-----TRIP LENGHT-----
+
 
 //get value from trip-range and print it to trip-leght
-let tripLengh = document.getElementById('trip-lenght');
-let tripRange = document.getElementById('trip-range');
-tripLengh.innerText=tripRange.value
+tripLengh.innerText = tripRange.value;
 
 //change trip-lenght on change of trip-range
-tripRange.addEventListener('change',function(){
-
-    tripLengh.innerText=tripRange.value;
+tripRange.addEventListener('change', function () {
+    tripLengh.innerText = tripRange.value;
 });
 
-document.getElementById('btn-reset').addEventListener('click', function(event){
-   
-    event.preventDefault();
-    tripRange.value=1
-    tripLengh.innerText=tripRange.value;
+
+//-----PRICES VARIABLES-----
+
+
+const kmPrice = 0.21;
+const discount18 = 20;
+const discount65 = 40;
+
+//-----SUBMIT-----
+
+
+let result = document.getElementById('result')
+const subBtn = document.querySelector('.sub-btn')
+
+subBtn.addEventListener('click', function (event) {
+    event.preventDefault()
+    // tripRange = Number(tripRange.value)
+
+    //define default final price w/o discount
+    let finalPrice = (kmPrice * Number(tripRange.value));
+
+    //validate age entry. print alert
+    if (age.value === '') {
+        result.innerText = 'Imposta un età'
+    }
+    else {
+        //check age and calculate final price
+        if (age.value === '18-') {
+            finalPrice -= ((kmPrice * Number(tripRange.value)) * discount18 / 100); //sostituire Number(tripRange.value) con variabile. non funziona (NaN al secondo utilizzo)
+        }
+        else if (age.value === '65+') {
+            finalPrice -= ((kmPrice * Number(tripRange.value)) * discount65 / 100);
+        }
+
+        result.innerText = 'Costo: ' + finalPrice.toFixed(2) + '€'
+    }
+
+    result.classList.remove('d-none')
 })
 
+//-----RESET BTN-----
 
+document
+    .getElementById('btn-reset')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+        tripRange.value = 1;
+        tripLengh.innerText = tripRange.value;
+        age.value = '';
+        firstName.value = '';
+        surname.value = '';
+        result.classList.add('d-none')
+    });
